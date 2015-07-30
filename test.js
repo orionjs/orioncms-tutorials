d514ea8248f8d12f403ea949166d726fcd1ab78f
Nicolás López <nicolaslopezj@me.com>
2015-07-30 12:25:44 -0300

    update tests

--- test.js
+++ test.js
@@ -21,4 +21,24 @@ describe('tutorials.json', function(){
       }
     }
   })
+  it('should not generate errors with highlightjs', function() {
+    var json = JSON.parse(file);
+
+    if (!Array.isArray(json)) {
+      throw Error('tutorials.json is not an array');
+    }
+
+    for (var i = 0; i < json.length; i++) {
+      var page = json[i];
+      var fileContents = fs.readFileSync('./tutorials/' + page.file, 'utf8');
+      if (!fileContents) {
+        throw Error('file "' + page.file + '" is empty');
+      }
+
+      var re = /\n```.*```/g;
+      if (re.test(fileContents)) {
+        throw Error('code without language should not start in a new line');
+      }
+    }
+  });
 })
